182514f64fd695b7102ac4835445ebc8996d951b
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-06 18:30:15 +0900

    renderの使い方を学習。

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def signupfunc(request):
+    return render(request, "signup.html", {"some": 100})  # 第三引数にhtmlへ渡す変数定数を書くことができる。
