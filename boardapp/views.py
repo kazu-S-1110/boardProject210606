480e5d2a01350c78d619280d27d97c9ce9542ccf
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-06 18:44:11 +0900

    signup.htmlを装飾。bootstrapのexampleを使用。

--- boardapp/views.py
+++ boardapp/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 
 def signupfunc(request):
-    return render(request, "signup.html", {"some": 100})  # 第三引数にhtmlへ渡す変数定数を書くことができる。
+    return render(request, "signup.html", {})  # 第三引数にhtmlへ渡す変数定数を書くことができる。
