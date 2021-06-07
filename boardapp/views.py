971229b118e316622fce85149421a14e7fb913e8
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-07 20:35:54 +0900

    methodの導入と受け取り方を学習。

--- boardapp/views.py
+++ boardapp/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 
 
 def signupfunc(request):
+    if request.method == "POST":  # methodを受け取ることが可能
+        print("this is post method")
+    else:
+        print("this is not post method")
     return render(request, "signup.html", {})  # 第三引数にhtmlへ渡す変数定数を書くことができる。
