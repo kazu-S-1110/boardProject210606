a4000e60ab067cabb527581c53f53764570ec016
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-07 20:54:26 +0900

    Userの使い方とプロパティの指定方法を学習。

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 
 def signupfunc(request):
+    object = User.objects.get(username="super")  # objects.allでUser全員を指定可能
+    print(object.date_joined)  # いろんなプロパティを指定ができるよ！
     if request.method == "POST":  # methodを受け取ることが可能
         print("this is post method")
     else:
