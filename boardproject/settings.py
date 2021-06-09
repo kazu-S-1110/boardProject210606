6da86653b00e55ad1b5ce466b13f971580012441
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-09 12:42:48 +0900

    static_rootなど静的ファイルの扱いを学習。

--- boardproject/settings.py
+++ boardproject/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-gbl++)rtv@bsi7&2-wifrmza_^ibujewl6(o&oyq!_cuwg!&6_
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'boardproject.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -80,7 +77,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -100,7 +96,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -114,14 +109,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/'  # アクセスする際のURL
+STATIC_ROOT = BASE_DIR / "staticfiles"  # デプロイした際の静的ファイルの保存先(開発時では使わない。static_dirsと同じ名前だとエラーを吐く。# )
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+STATICFILES_DIRS = [str(BASE_DIR / "static")]  # 静的ファイルの保存先は開発時、複数あると便利な場合がある。その際に書くコード。
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
