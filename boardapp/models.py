4570396c717ce126e9085a9cf6dba898d568aea5
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-12 21:59:21 +0900

    modelのnull,blankを学習、実装。

--- boardapp/models.py
+++ boardapp/models.py
@@ -5,7 +5,7 @@ class BoardModel(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     contributor = models.CharField(max_length=30)
-    user_image = models.ImageField(upload_to="") # upload_to は画像の置き場を指定する。置き場の基準はsettings.pyで指定している。
-    good = models.IntegerField()
-    read = models.IntegerField()
-    readtext = models.TextField()
+    user_image = models.ImageField(upload_to="")  # upload_to は画像の置き場を指定する。置き場の基準はsettings.pyで指定している。
+    good = models.IntegerField(null=True, blank=True, default=0)
+    read = models.IntegerField(null=True, blank=True, default=0)
+    readtext = models.TextField(null=True, blank=True, default="")
